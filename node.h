1b67fd4f28837bcfb956f36babeda6dce767df37
Franco P <franco.pacheco@utec.edu.pe>
2022-04-19 22:08:45 -0500

    todo okay

---
+++ node.h
@@ -0,0 +1,27 @@
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
+template <typename T>
+struct Node {
+    T data;
+    Node<T>* next;
+    Node<T>* prev;
+
+    Node(){ 
+        //data = 0;
+        next = nullptr;
+        prev = nullptr;
+    }
+
+    Node(T value){
+        data = value;
+        next = nullptr;
+        prev = nullptr;
+    }
+
+    void killSelf(){
+        delete this;
+    }    
+};
+
+#endif
